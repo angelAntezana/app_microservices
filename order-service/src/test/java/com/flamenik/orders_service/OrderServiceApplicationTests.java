dd4f53f45bb40a24ec46f8f825718e08e5f97345
angelAntezana <angel2estevan@gmail.com>
2024-01-06 14:25:49 +0100

    Added modules

---
+++ order-service/src/test/java/com/flamenik/orders_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flamenik.orders_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
